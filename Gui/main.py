7bb7c16a91a74ad344acc0c2a30895127756a880
mrog357 <s184435@student.pg.edu.pl>
2023-03-27 18:18:38 +0200

    Initial commit

---
+++ Gui/main.py
@@ -0,0 +1,105 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
+
+
+class MainWindow():
+
+    def __init__(self, mainWidget):
+        self.main_frame = ttk.Frame(mainWidget, width=1200, height=800, padding=(0, 0, 0, 0))
+        self.main_frame.grid(row=0, column=0)
+        self.out_of_main = False;
+        image1 = Image.open("karaoke-logo.png")
+        image2 = image1.resize((200, 200))
+        self.test = ImageTk.PhotoImage(image2)
+
+        self.main_gui()
+
+    def main_gui(self):
+        root.title('My Little Karaoke')
+        root.configure(bg='#E61E4F')
+
+
+        self.logo = ttk.Label(self.main_frame, image=self.test)
+
+        self.logo.image = self.test
+        self.logo.configure(background='#E61E4F')
+
+        # Position image
+        self.logo.grid(row=1, column=1)
+
+
+        self.create_button = ttk.Button(self.main_frame, text='Utwórz nowe karaoke')
+        self.create_button.grid(row=2, column=1)
+        self.create_button.bind('<Button-1>', self.create_gui)
+
+        self.archive_button = ttk.Button(self.main_frame, text='Otwórz archiwum')
+        self.archive_button.grid(row=2, column=3)
+        self.archive_button.bind('<Button-1>', self.archive_gui)
+
+        self.gui_elements = [self.create_button, self.logo, self.archive_button]
+
+    def create_gui(self, event):
+
+        self.gui_elements_remove(self.gui_elements)
+        self.out_of_main = True
+
+        self.back_to_main_button = ttk.Button(self.main_frame, text='Cofnijl')
+        self.back_to_main_button.grid(row=0, column=0)
+        self.back_to_main_button.bind('<Button-1>', self.back_to_main)
+
+        self.gui_elements = [self.back_to_main_button]
+
+    def archive_gui(self, event):
+
+        self.gui_elements_remove(self.gui_elements)
+        self.out_of_main = True
+
+        self.back_to_main_button = ttk.Button(self.main_frame, text='Cofnij')
+        self.back_to_main_button.grid(row=0, column=0)
+        self.back_to_main_button.bind('<Button-1>', self.back_to_main)
+
+        self.gui_elements = [self.back_to_main_button]
+
+    def setings_gui(self, event):
+        self.gui_elements_remove(self.gui_elements)
+
+        root.title('Setings')
+
+        self.main_label_1 = ttk.Label(self.main_frame, text='Object_1')
+        self.main_label_1.grid(row=2, column=0)
+
+        self.main_menu_button = ttk.Button(self.main_frame, text='Main menu')
+        self.main_menu_button.grid(row=0, column=1)
+        self.main_menu_button.bind('<Button-1>', self.back_to_main)
+
+        self.out_of_main = 1
+
+        self.gui_elements = [self.main_label_1,
+                             self.main_menu_button]
+
+    def back_to_main(self, event):
+        if self.out_of_main == True:
+            self.gui_elements_remove(self.gui_elements)
+        else:
+            pass
+        self.main_gui()
+
+    def gui_elements_remove(self, elements):
+        for element in elements:
+            element.destroy()
+
+def main():
+    global root
+
+    root = Tk()
+    root.geometry('1000x600+100+100')
+    root.title("My Little Karaoke")
+    icon = PhotoImage(file='karaoke-logo.png')
+    root.iconphoto(False, icon)
+    window = MainWindow(root)
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
